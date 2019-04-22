e8bb7333e7905388ebf4fd23315fc1947f592920
MrYacha <YachaGorobtsov@gmail.com>
2019-04-22 15:50:44 +0300

    Added /sbroadcast (Smart Broadcast)
    It send broadcasted message after someone write message in group, after remove group id from list

--- sophie_bot/modules/bot_admin.py
+++ sophie_bot/modules/bot_admin.py
@@ -69,3 +69,47 @@ async def event(event):
             num_succ, num_fail
         )) 
     
+
+
+@register(incoming=True, pattern="^/sbroadcast ?(.*)")
+async def event(event):
+    if event.from_id not in OWNER_ID:
+        return
+    text = event.pattern_match.group(1)
+    # Add chats to sbroadcast list
+    chats = MONGO.chat_list.find({})
+    MONGO.sbroadcast_list.drop()
+    MONGO.sbroadcast_settings.drop()
+    for chat in chats:
+        MONGO.sbroadcast_list.insert_one({'chat_id': chat['chat_id']})
+    MONGO.sbroadcast_settings.insert_one({
+        'text': text,
+        'all_chats': chats.count(),
+        'recived_chats': 0
+    })
+    await event.reply("Smart broadcast planned for `{}` chats".format(chats.count()))
+
+# Check on smart broadcast 
+@register(incoming=True)
+async def event(event):
+    chat_id = event.chat_id
+    match = MONGO.sbroadcast_list.find_one({'chat_id': chat_id})
+    if match:
+        print("yes")
+        try:
+            raw_text = MONGO.sbroadcast_settings.find_one({})['text']
+            print(raw_text)
+            text, buttons = button_parser(event.chat_id, raw_text)
+            print(buttons)
+            await bot.send_message(chat_id, text, buttons=buttons)
+        except Exception as err:
+            print(err)
+        MONGO.sbroadcast_list.delete_one({'chat_id': chat_id})
+        old = MONGO.sbroadcast_settings.find_one({})
+        num = old['recived_chats'] + 1
+        MONGO.sbroadcast_settings.update(
+            {'_id':old['_id']}, {
+                'text': old['text'],
+                'all_chats': old['all_chats'],
+                'recived_chats': num
+            }, upsert=False)
