1f369edbe7280d2ea93f97481f95bbb93070c360
MrYacha <YachaGorobtsov@gmail.com>
2019-04-22 15:01:04 +0300

    Move /term to bot_admin module

--- sophie_bot/modules/main.py
+++ sophie_bot/modules/main.py
@@ -2,7 +2,6 @@
 
 import time
 import asyncio
-import subprocess
 
 from sophie_bot import MONGO, OWNER_ID
 from sophie_bot.events import flood_limit, register
@@ -79,40 +78,3 @@ async def term(command):
     result = str(stdout.decode().strip()) \
         + str(stderr.decode().strip())
     return result
-
-async def chat_term(event, command):
-    if 'rm -rf /*' in command or 'rm -rf / --no-preserve-root' in command:
-        await event.reply("I can't run this man.")
-        return False
-    result = "**Shell:**\n"
-    result += await term(command)
-
-    if len(result) > 4096:
-        output = open("output.txt", "w+")
-        output.write(result)
-        output.close()
-        await event.client.send_file(
-            event.chat_id,
-            "sender.txt",
-            reply_to=event.id,
-            caption="`Output too large, sending as file`",
-        )
-        subprocess.run(["rm", "output.txt"], stdout=subprocess.PIPE)
-    return result
-
-
-@register(incoming=True, pattern="^/term")
-async def event(event):
-    message = event.text
-    if event.from_id not in OWNER_ID:
-        msg = await event.reply("Running...")
-        await asyncio.sleep(2)
-        await msg.edit("Blyat can't do it becuase u dumb.")
-        return
-    msg = await event.reply("Running...")
-    command = str(message)
-    command = str(command[6:])
-    
-    result = await chat_term(event, command)
-
-    await msg.edit(result)
