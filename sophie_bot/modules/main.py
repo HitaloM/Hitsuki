28fce48514469af91be1e0d3dc03573926c437be
MrYacha <YachaGorobtsov@gmail.com>
2019-04-29 01:37:41 +0300

    Move /id to misc module; Fix giving info on replied message

--- sophie_bot/modules/main.py
+++ sophie_bot/modules/main.py
@@ -5,8 +5,8 @@ import asyncio
 import math
 
 from sophie_bot import MONGO, OWNER_ID
+from sophie_bot.modules.users import get_user_and_text, user_link
 from sophie_bot.events import flood_limit, register
-from sophie_bot.modules.users import user_link
 
 from telethon import custom
 
@@ -33,44 +33,6 @@ Please wait 3 minutes before using this command')
                custom.Button.url('Channel', 't.me/ok')]]
 
     await event.reply(text, buttons=inline)
-
-
-@register(incoming=True, pattern="^/id ?(.*)")
-async def event(event):
-
-    res = flood_limit(event.chat_id, 'id')
-    if res == 'EXIT':
-        return
-    elif res is True:
-        await event.reply('**Flood detected! **\
-Please wait 3 minutes before using this command')
-        return
-
-    text = "**ID's:**\n"
-    text += "Your id - `{}`\n".format(event.from_id)
-    text += "Chat id - `{}`\n".format(event.chat_id)
-    text += "Your message id - `{}`\n".format(event.message.id)
-
-    #TODO: Normal args...
-    if event.message.text == '/id':
-        await event.reply(text)
-        return
-
-    elif event.message.reply_to_msg_id:
-        msg = await event.get_reply_message()
-        text += "\n**Replied message:**\n"
-        user = MONGO.user_list.find_one({'user_id': msg.from_id})
-        userl = await user_link(msg.from_id)
-        text += "{}'s user id - `{}`\n".format(userl, msg.from_id)
-        text += "{}'s message id - `{}`".format(userl, msg.id)
-
-    else:
-        user, lol = await get_user_and_text(event)
-        lol = lol # No prevent pylint warn ;-;
-        user_link = "[{}](https://t.me/{})'s".format(user['first_name'], user['user_id'])
-        text += "{} user id - `{}`\n".format(user_link, user['user_id'])
-    
-    await event.reply(text)
     
 
 async def term(command):
---
+++ sophie_bot/modules/misc.py
@@ -0,0 +1,41 @@
+from sophie_bot import MONGO
+
+from sophie_bot.modules.users import get_user_and_text, user_link
+from sophie_bot.events import flood_limit, register
+
+
+@register(incoming=True, pattern="^/id ?(.*)")
+async def event(event):
+
+    res = flood_limit(event.chat_id, 'id')
+    if res == 'EXIT':
+        return
+    elif res is True:
+        await event.reply('**Flood detected! **\
+Please wait 3 minutes before using this command')
+        return
+
+    text = "**ID's:**\n"
+    text += "Your id - `{}`\n".format(event.from_id)
+    text += "Chat id - `{}`\n".format(event.chat_id)
+    text += "Your message id - `{}`\n".format(event.message.id)
+
+    if event.message.reply_to_msg_id:
+        msg = await event.get_reply_message()
+        text += "\n**Replied message:**\n"
+        user = MONGO.user_list.find_one({'user_id': msg.from_id})
+        userl = await user_link(msg.from_id)
+        text += "{}'s user id - `{}`\n".format(userl, msg.from_id)
+        text += "{}'s message id - `{}`".format(userl, msg.id)
+
+    elif event.message.raw_text == "/id":
+        await event.reply(text)
+        return
+
+    else:
+        user, lol = await get_user_and_text(event)
+        lol = lol # No prevent pylint warn ;-;
+        userl = "[{}](https://t.me/{})'s".format(user['first_name'], user['user_id'])
+        text += "{} user id - `{}`\n".format(userl, user['user_id'])
+    
+    await event.reply(text)
