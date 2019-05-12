f7012d1eb3279166de6dbb6db09f1c60a92b0bcd
MrYacha <YachaGorobtsov@gmail.com>
2019-05-12 23:44:03 +0300

    Cleanup

--- sophie_bot/__main__.py
+++ sophie_bot/__main__.py
@@ -1,16 +1,10 @@
 from importlib import import_module
 
-from sophie_bot import TOKEN, bot, logger, SUDO, WHITELISTED
+from sophie_bot import TOKEN, bot, logger
 from sophie_bot.modules import ALL_MODULES
 
 for module_name in ALL_MODULES:
     imported_module = import_module("sophie_bot.modules." + module_name)
-
-logger.info("Sudo list is loading")
-logger.info(SUDO)
-
-logger.info("Whitelist list is loading")
-logger.info(WHITELISTED)
 
 logger.info("Modules loaded!")
 
