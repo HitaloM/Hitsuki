2a0ad07168ff5cacda14455b1dd6920f4596da11
MrYacha <YachaGorobtsov@gmail.com>
2019-05-24 11:12:20 +0300

    Implement Sed Command

---
+++ sophie_bot/modules/components/sed.py
@@ -0,0 +1,90 @@
+import re
+from collections import defaultdict, deque
+
+from sophie_bot import bot
+
+from telethon import events
+
+SED_PATTERN = r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(/.*)?'
+
+last_msgs = defaultdict(lambda: deque(maxlen=10))
+
+
+async def doit(message, match):
+    fr = match.group(1)
+    to = match.group(2)
+    to = (to
+          .replace('\\/', '/')
+          .replace('\\0', '\\g<0>'))
+    try:
+        fl = match.group(3)
+        if fl is None:
+            fl = ''
+        fl = fl[1:]
+    except IndexError:
+        fl = ''
+
+    # Build Python regex flags
+    count = 1
+    flags = 0
+    for f in fl.lower():
+        if f == 'i':
+            flags |= re.IGNORECASE
+        elif f == 'm':
+            flags |= re.MULTILINE
+        elif f == 's':
+            flags |= re.DOTALL
+        elif f == 'g':
+            count = 0
+        elif f == 'x':
+            flags |= re.VERBOSE
+        else:
+            await message.reply('unknown flag: {}'.format(f))
+            return
+
+    def substitute(m):
+        if not m.raw_text:
+            return None
+
+        s, i = re.subn(fr, to, m.raw_text, count=count, flags=flags)
+        if i > 0:
+            return s
+
+    try:
+        substitution = None
+        if message.is_reply:
+            substitution = substitute(await message.get_reply_message())
+        else:
+            for msg in reversed(last_msgs[message.chat_id]):
+                substitution = substitute(msg)
+                if substitution is not None:
+                    break
+
+        if substitution is not None:
+            await message.reply(substitution)
+
+    except Exception as e:
+        await message.reply('fuck me\n' + str(e))
+
+
+@bot.on(events.NewMessage(pattern=SED_PATTERN))
+@bot.on(events.MessageEdited(pattern=SED_PATTERN))
+async def sed(event):
+    message = await doit(event.message, event.pattern_match)
+    if message:
+        last_msgs[event.chat_id].append(message)
+
+    # Don't save sed commands or we would be able to sed those
+    raise events.StopPropagation
+
+
+@bot.on(events.NewMessage)
+async def catch_all(event):
+    last_msgs[event.chat_id].append(event.message)
+
+
+@bot.on(events.MessageEdited)
+async def catch_edit(event):
+    for i, message in enumerate(last_msgs[event.chat_id]):
+        if message.id == event.id:
+            last_msgs[event.chat_id][i] = event.message
