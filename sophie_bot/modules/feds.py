ebbc251cda7873ec295ce3802c4f1e35f1b7545b
MrYacha <YachaGorobtsov@gmail.com>
2019-05-31 12:20:02 +0300

    Cooked /fpromote

--- sophie_bot/modules/feds.py
+++ sophie_bot/modules/feds.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantCreator
 
 from sophie_bot import bot, decorator, mongodb
 from sophie_bot.modules.language import get_string, get_strings_dec
-from sophie_bot.modules.users import user_link
+from sophie_bot.modules.users import user_link, get_user_and_text, user_link
 
 
 @decorator.command('newfed', arg=True)
@@ -37,7 +37,7 @@ async def join_fed_comm(event, strings):
 
 @decorator.command('leavefed')
 @get_strings_dec("feds")
-async def join_fed_comm(event, strings):
+async def leave_fed_comm(event, strings):
     chat = event.chat_id
     user = event.from_id
     if await leave_fed(event, chat, user) is True:
@@ -45,7 +45,6 @@ async def join_fed_comm(event, strings):
 
 
 async def join_fed(event, chat_id, fed_id, user):
-
     peep = await bot(
         GetParticipantRequest(
             channel=chat_id, user_id=user,
@@ -66,13 +65,12 @@ async def join_fed(event, chat_id, fed_id, user):
         return
 
     join_data = {'chat_id': chat_id, 'fed_id': fed_id}
-
     mongodb.fed_groups.insert_one(join_data)
+
     return True
 
 
 async def leave_fed(event, chat_id, user):
-
     peep = await bot(
         GetParticipantRequest(
             channel=chat_id, user_id=user,
@@ -83,9 +81,40 @@ async def leave_fed(event, chat_id, user):
         return
 
     old = mongodb.fed_groups.delete_one({'chat_id': chat_id}).deleted_count
-
     if old < 1:  # If chat not was in any federation
-        await event.reply(get_string('feds', 'leaved_fed_already', chat_id))
+        await event.reply(get_string('feds', 'chat_not_in_fed', chat_id))
         return
 
     return True
+
+
+@decorator.command('fpromote', arg=True)
+@get_strings_dec("feds")
+async def promote_to_fed(event, strings):
+    chat_id = event.chat_id
+    user_id = event.from_id
+    user, fed_id = await get_user_and_text(event)
+    if not fed_id:
+        chat_fed = mongodb.fed_groups.find_one({'chat_id': chat_id})
+        if not chat_fed:
+            await event.reply(strings['chat_not_in_fed'])
+            return
+        fed = mongodb.fed_list.find_one({'fed_id': chat_fed['fed_id']})
+    else:
+        fed = mongodb.fed_list.find_one({'fed_id': fed_id.lower()})
+        if not fed:
+            await event.reply(strings['fed_id_invalid'])
+            return
+    if not user_id == fed["creator"]:
+        await event.reply(strings["only_creator_promote"])
+        return
+    data = {'fed_id': fed['fed_id'], 'admin': user['user_id']}
+
+    old = mongodb.fed_admins.find_one(data)
+    if old:
+        await event.reply(strings["admin_already_in_fed"].format(
+            user=await user_link(user['user_id']), name=fed['fed_name']))
+        return
+    mongodb.fed_admins.insert_one(data)
+    await event.reply(strings["admin_added_to_fed"].format(
+        user=await user_link(user['user_id']), name=fed['fed_name']))
