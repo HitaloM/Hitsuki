7c38cd0fb86cbe343da9fc70d6e63b82f13b4730
MrYacha <YachaGorobtsov@gmail.com>
2019-06-03 01:26:27 +0300

    Update OrangeFox component

--- sophie_bot/modules/components/fox.py
+++ sophie_bot/modules/components/fox.py
@@ -1,17 +1,14 @@
-import re
+from ftplib import FTP
 
-import requests
-from bs4 import BeautifulSoup
 from telethon import custom
 
-from sophie_bot import decorator, logger
+from sophie_bot import SUDO, CONFIG, decorator, logger
+from sophie_bot.modules.users import user_link
 from sophie_bot.modules.helper_func.flood import flood_limit_dec
 
-beta_url = 'https://files.orangefox.website/OrangeFox Beta/'
-stable_url = 'https://files.orangefox.website/OrangeFox-Stable/'
-ext = 'zip'
-fox_groups = [483808054, -1001287179850, -1001280218923, -1001155400138]
-fox_beta_groups = [483808054, -1001280218923]
+ftp_url = "ftp.orangefox.website"
+fox_groups = [483808054, -1001287179850, -1001280218923, -1001155400138, -1001362128194]
+fox_beta_groups = [483808054, -1001280218923, -1001362128194]
 
 global DEVICES_STABLE
 global DEVICES_BETA
@@ -21,144 +18,136 @@ DEVICES_BETA = {}
 
 
 def update_devices():
+    logger.info("Update info about OrangeFox builds..")
     global DEVICES_STABLE
     global DEVICES_BETA
 
-    response_stable = requests.get(stable_url, params={})
-    if not response_stable.ok:
-        logger.error(response_stable.raise_for_status())
-        return
-    response_beta = requests.get(beta_url, params={})
-    if not response_beta.ok:
-        logger.error(response_beta.raise_for_status())
-        return
+    ftp = FTP(ftp_url, CONFIG['advanced']['ofox_ftp_user'], CONFIG['advanced']['ofox_ftp_pass'])
 
-    soup_stable = BeautifulSoup(response_stable.text, 'html.parser')
-    soup_beta = BeautifulSoup(response_beta.text, 'html.parser')
-
-    stable_devices_list = re.findall(
-        r'<a href="/OrangeFox-Stable/(.+?)/">.+?</a>', str(soup_stable))
-    beta_devices_list = re.findall(r'<a href="/OrangeFox%20Beta/(.+?)/">.+?</a>', str(soup_beta))
-
-    for device in stable_devices_list:
-
-        link = stable_url + device
-
-        raw = requests.get(link, params={})
-        if not raw.ok:
-            logger.error(raw)
+    data = ftp.mlsd("OrangeFox-Stable", ["type"])
+    for device, facts in data:
+        if not facts["type"] == "dir":
             continue
-        text = BeautifulSoup(raw.text, 'html.parser')
-        builds = re.search(r"OrangeFox-(\w*)-(\w*)-(\w*)-(\w*)\.zip", str(text))
-        if not builds:
-            builds = re.search(r"OrangeFox-(\w*)-(\w*)-(\w*)\.zip", str(text))
-            if not builds:
-                builds = re.search(r">OrangeFox-(.*)\.zip", str(text))
-        
-        if builds[0] == ">":
-            builds = builds[1:]
 
-        link = stable_url + device + '/device_info.txt'
-        raw = requests.get(link, params={})
-        if not raw.ok:
-            logger.error(raw)
-            continue
-        text = BeautifulSoup(raw.text, 'html.parser')
-        codename = re.search(r"Codename: (.*)", str(text)).group(1)
-        fullname = re.search(r"Device name: (.*)", str(text)).group(1)
-        maintainer = re.search(r"Maintainer: (.*)", str(text)).group(1)
+        info_file = []
+        ftp.retrlines(f'RETR OrangeFox-Stable/{device}/device_info.txt', info_file.append)
+
+        codename = info_file[0].split(': ')[1]
+        fullname = info_file[1].split(': ')[1]
+        maintainer = info_file[2].split(': ')[1]
+
+        builds = list(ftp.mlsd("OrangeFox-Stable/" + device, ["type"]))
+        builds.sort(key=lambda entry: entry[1]['modify'], reverse=True)
+        for build, facts in builds:
+            if not facts["type"] == "file":
+                continue
+            if build == "device_info.txt":
+                continue
+            last_build = build
+            modified = facts['modify']
+            break
 
         DEVICES_STABLE[device] = {
             "codename": codename,
             "fullname": fullname,
             "maintainer": maintainer,
-            "ver": builds.group()
+            "ver": last_build,
+            "modified": modified
         }
 
-    for device in beta_devices_list:
-        link = beta_url + device
+    data = ftp.mlsd("OrangeFox Beta", ["type"])
+    for device, facts in data:
+        if not facts["type"] == "dir":
+            continue
 
-        raw = requests.get(link, params={})
-        if not raw.ok:
-            logger.error(raw)
-            continue
-        text = BeautifulSoup(raw.text, 'html.parser')
-        builds = re.search(r"OrangeFox-(\w*)-(\w*)-(\w*)-(\w*)\.zip", str(text))
-        if not builds:
-            builds = re.search(r"OrangeFox-(\w*)-(\w*)-(\w*)\.zip", str(text))
-        link = beta_url + device + '/device_info.txt'
-        raw = requests.get(link, params={})
-        if not raw.ok:
-            logger.error(raw)
-            continue
-        text = BeautifulSoup(raw.text, 'html.parser')
-        codename = re.search(r"Codename: (.*)", str(text)).group(1)
-        fullname = re.search(r"Device name: (.*)", str(text)).group(1)
-        maintainer = re.search(r"Maintainer: (.*)", str(text)).group(1)
+        info_file = []
+        ftp.retrlines(f'RETR OrangeFox Beta/{device}/device_info.txt', info_file.append)
+
+        codename = info_file[0].split(': ')[1]
+        fullname = info_file[1].split(': ')[1]
+        maintainer = info_file[2].split(': ')[1]
+
+        builds = list(ftp.mlsd("OrangeFox Beta/" + device, ["type"]))
+        builds.sort(key=lambda entry: entry[1]['modify'], reverse=True)
+        for build, facts in builds:
+            if not facts["type"] == "file":
+                continue
+            if build == "device_info.txt":
+                continue
+            last_build = build
+            modified = facts['modify']
+            break
 
         DEVICES_BETA[device] = {
             "codename": codename,
             "fullname": fullname,
             "maintainer": maintainer,
-            "ver": builds.group()
+            "ver": last_build,
+            "modified": modified
         }
+    logger.info("Done!")
 
 
 # Main
-logger.info("Update info about OrangeFox builds..")
 update_devices()
-logger.info("Done!")
 print(DEVICES_STABLE)
 print(DEVICES_BETA)
 
 
-@decorator.command("listbeta")
-@flood_limit_dec("listbeta")
-async def listbeta(event):
-    if event.chat_id not in fox_groups:
-        return
-    text = "**Supported beta devices:**\n"
-    for device in DEVICES_BETA:
-        text += "* {} (`{}`)\n".format(
-            DEVICES_BETA[device]['fullname'], DEVICES_BETA[device]["codename"])
-    text += "\nTo get device write `/codename`"
-    await event.reply(text)
+@decorator.command("update", from_users=SUDO)
+async def update_devices(event):
+    update_devices()
+    await event.reply("Done")
 
 
 @decorator.command("list")
 @flood_limit_dec("list")
 async def list_stable(event):
-    if event.chat_id not in fox_groups:
-        return
-    text = "**Supported devices:**\n"
-    for device in DEVICES_STABLE:
-        text += "* {} (`{}`)\n".format(
-            DEVICES_STABLE[device]['fullname'], DEVICES_STABLE[device]["codename"])
+    if event.chat_id in fox_beta_groups:
+        text = "**Beta testing devices:**\n"
+        for device in DEVICES_BETA:
+            text += "* {} (`{}`)\n".format(
+                DEVICES_BETA[device]['fullname'], DEVICES_BETA[device]["codename"])
+    elif event.chat_id in fox_groups:
+        text = "**Supported devices:**\n"
+        for device in DEVICES_STABLE:
+            text += "* {} (`{}`)\n".format(
+                DEVICES_STABLE[device]['fullname'], DEVICES_STABLE[device]["codename"])
     text += "\nTo get device write `/codename`"
     await event.reply(text)
 
 
 @decorator.StrictCommand("^[/#](.*)")
 async def check(event):
-    if event.chat_id not in fox_groups:
-        return
     device_arg = event.pattern_match.group(1).lower()
-    if device_arg not in DEVICES_STABLE:
+    if device_arg not in DEVICES_STABLE and device_arg not in DEVICES_BETA:
         return
 
-    device = DEVICES_STABLE[device_arg]
-    beta_device = DEVICES_BETA[device_arg]
-    text = "**" + device['fullname'] + "** (`{}`)".format(device['codename'])
-    text += "\nMaintainer: " + beta_device['maintainer']
     if event.chat_id in fox_beta_groups and device_arg in DEVICES_BETA:
+        beta_device = DEVICES_BETA[device_arg]
+        text = "**" + beta_device['fullname'] + "** (`{}`)".format(beta_device['codename'])
         build = beta_device['ver']
         text += "\nLast beta: `" + build + "`"
+        text += "\nDate: `" + beta_device['modified'] + "`"
+        if beta_device['maintainer'].isdigit():
+            maintainer = await user_link(int(beta_device['maintainer']))
+        else:
+            maintainer = beta_device['maintainer']
+        text += "\nMaintainer: " + maintainer
         link_beta = "https://files.orangefox.website/OrangeFox%20Beta/" + device_arg
         buttons = [[custom.Button.url("Download beta", link_beta + "/" + build),
                     custom.Button.url("All builds", link_beta)]]
-    else:
+    elif event.chat_id in fox_groups and device_arg in DEVICES_STABLE:
+        device = DEVICES_STABLE[device_arg]
+        text = "**" + device['fullname'] + "** (`{}`)".format(device['codename'])
         build = device['ver']
         text += "\nLast build: `" + build + "`"
+        text += "\nDate: `" + device['modified'] + "`"
+        if beta_device['maintainer'].isdigit():
+            maintainer = await user_link(int(beta_device['maintainer']))
+        else:
+            maintainer = beta_device['maintainer']
+        text += "\nMaintainer: " + maintainer
         link_stable = "https://files.orangefox.website/OrangeFox-Stable/" + device_arg
         buttons = [[custom.Button.url("Download last", link_stable + "/" + build)]]
         link_mirror = "https://sourceforge.net/projects/orangefox/files/"
