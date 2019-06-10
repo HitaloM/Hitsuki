5892c24eaa31e837f32a22f71955a09b2333aef5
MrYacha <YachaGorobtsov@gmail.com>
2019-06-10 21:43:59 +0300

    Fix not_load_this_components settings if list contain many components

--- sophie_bot/modules/components/__init__.py
+++ sophie_bot/modules/components/__init__.py
@@ -7,19 +7,23 @@ NO_LOAD_COMPONENTS = CONFIG["advanced"]["not_load_this_components"]
 
 
 def list_all_components():
+    components = []
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_components = [
         basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
-    return all_components
+    for component in all_components:
+        if component not in NO_LOAD_COMPONENTS:
+            components.append(component)
+
+    return components
 
 
-ALL_COMPONENTS = sorted(list_all_components())
+ALL_COMPONENTS = sorted(list(list_all_components()))
 
-for component in ALL_COMPONENTS:
-    if component in NO_LOAD_COMPONENTS:
-        ALL_COMPONENTS.remove(component)
+print(ALL_COMPONENTS)
+
 logger.info("Components to load: %s", str(ALL_COMPONENTS))
 __all__ = ALL_COMPONENTS + ["ALL_COMPONENTS"]
