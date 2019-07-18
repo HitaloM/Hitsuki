42b0734800163b9ca164c0b0a735e6e12755a7c8
pqhaz <47860146+pqhaz@users.noreply.github.com>
2019-07-18 10:15:16 +0300

    Rewrite rules, notes for AIO

--- sophie_bot/modules/rules.py
+++ sophie_bot/modules/rules.py
@@ -1,46 +1,66 @@
-from aiogram import types
-
 from sophie_bot import decorator, mongodb
 from sophie_bot.modules.connections import connection
 from sophie_bot.modules.language import get_strings_dec
 from sophie_bot.modules.users import user_admin_dec
+from sophie_bot.modules.notes import send_note
 
 
 @decorator.command("setrules")
 @user_admin_dec
 @connection(only_in_groups=True, admin=True)
 @get_strings_dec("rules")
-async def setwelcome(message, strings, status, chat_id, chat_title, *args):
-    chat = mongodb.chat_list.find_one({'chat_id': int(chat_id)})
-    arg = message['text'].split(" ", 2)
-    if len(arg) <= 1:
-        return
-    if status is False:
-        await message.reply(chat_id)
+async def setrules(message, strings, status, chat_id, chat_title, **kwargs):
+    note_to_find = message['text'][10:]
 
-    note_name = arg[1]
-    off = ['off', 'none', 'disable']
-    if note_name in off:
-        check = mongodb.rules.delete_one({'chat_id': chat})
-        if check:
-            text = "Rules deleted for this chat"
-        else:
-            text = "Rules didn't setuped yet"
-        await message.reply(text)
-        return
-    note = mongodb.notes.find_one({
-        'chat_id': chat_id,
-        'name': note_name
-    })
+    note = mongodb.notes.find_one({'chat_id': chat_id, 'name': note_to_find})
+
     if not note:
-        await message.reply(strings["cant_find_note"])
-        return
-    old = mongodb.rules.find_one({'chat_id': chat_id})
-    if old:
-        mongodb.rules.delete_one({'_id': old['_id']})
-    mongodb.rules.insert_one({
-        'chat_id': chat_id,
-        'note': note_name
-    })
-    await message.reply(strings["rules_set_to_note"].format(note_name),
-                        parse_mode=types.ParseMode.HTML)
+        return await message.reply(strings['cannot_find_note'])
+
+    in_db = mongodb.rules.find_one({'chat_id': chat_id})
+
+    if not in_db:
+        mongodb.rules.insert_one({
+            'chat_id': chat_id,
+            'note_name': note_to_find
+        })
+    else:
+        mongodb.rules.update_one({'chat_id': chat_id}, {
+            "$set": {
+                'note_name': note_to_find
+            }
+        })
+
+    return await message.reply(strings['set_note'].format(note_to_find))
+
+
+@decorator.command("rules")
+@connection(only_in_groups=True)
+@get_strings_dec("rules")
+async def rules(message, strings, status, chat_id, chat_title, **kwargs):
+    in_db = mongodb.rules.find_one({'chat_id': chat_id})
+
+    if not in_db:
+        return await message.reply(strings['didnt_set_note'])
+
+    note_in_db = mongodb.notes.find_one({'chat_id': chat_id, 'name': in_db['note_name']})
+
+    if not note_in_db:
+        return await message.reply(strings['cannot_find_set_note'])
+
+    await send_note(chat_id, chat_id, message.message_id, in_db['note_name'])
+
+
+@decorator.command("delrules")
+@user_admin_dec
+@connection(only_in_groups=True, admin=True)
+@get_strings_dec("rules")
+async def delrules(message, strings, status, chat_id, chat_title, **kwargs):
+    in_db = mongodb.rules.find_one({'chat_id': chat_id})
+
+    if not in_db:
+        return await message.reply(strings['havent_set_rules_note'])
+
+    mongodb.rules.delete_one({'chat_id': chat_id})
+
+    await message.reply(strings['success_remove_rules_note'].format(chat_title))
