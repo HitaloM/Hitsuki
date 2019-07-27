44c73bc3c6e40a5f1c16204d91388b28d75ff095
pqhaz <usaguy1342@gmail.com>
2019-07-27 16:47:51 +0000

    filters: Add warn action
    Signed-off-by: pqhaz <usaguy1342@gmail.com>

--- sophie_bot/modules/filters.py
+++ sophie_bot/modules/filters.py
@@ -1,13 +1,16 @@
 import re
 
-from sophie_bot import decorator, mongodb, redis
-from sophie_bot.modules.connections import connection
+from telethon.tl.custom import Button
+
+from sophie_bot import decorator, mongodb, redis, WHITELISTED
+from sophie_bot.modules.connections import connection, get_conn_chat
 from sophie_bot.modules.disable import disablable_dec
 from sophie_bot.modules.helper_func.flood import flood_limit_dec
 from sophie_bot.modules.language import get_string, get_strings_dec
 from sophie_bot.modules.notes import send_note, button_parser
 from sophie_bot.modules.bans import ban_user, kick_user
-from sophie_bot.modules.users import user_admin_dec, user_link
+from sophie_bot.modules.users import user_admin_dec, user_link, get_chat_admins
+from sophie_bot.modules.warns import randomString
 
 
 @decorator.insurgent()
@@ -62,6 +65,74 @@ async def check_message(event):
                         filter=filter
                     )
                     event.reply(text)
+            elif action == 'warn':
+                user_id = event.sender_id
+                if user_id in WHITELISTED:
+                    return
+
+                if user_id in await get_chat_admins(chat):
+                    return
+
+                warn_limit = mongodb.warnlimit.find_one({'chat_id': chat})
+                db_warns = mongodb.warns.find({
+                    'user_id': user_id,
+                    'group_id': chat
+                })
+
+                #  to avoid adding useless another warn in db
+                current_warns = 1
+
+                for _ in db_warns:
+                    current_warns += 1
+
+                if not warn_limit:
+                    warn_limit = 3
+                else:
+                    warn_limit = int(warn_limit['num'])
+
+                if current_warns >= warn_limit:
+                    if await ban_user(event, user_id, chat, None) is False:
+                        print(f'cannot ban user {user_id}')
+                        return
+
+                    await ban_user(event, user_id, chat, None, no_msg=False)
+                    mongodb.warns.delete_many({
+                        'user_id': user_id,
+                        'group_id': chat
+                    })
+
+                    resp = get_string("filters", "filter_warn_ban", event.chat_id).format(warns=warn_limit,
+                                                                                          user=await user_link(user),
+                                                                                          reason=H['arg'])
+
+                    return await event.reply(resp)
+                else:
+                    rndm = randomString(15)
+
+                    mongodb.warns.insert_one({
+                        'warn_id': rndm,
+                        'user_id': user_id,
+                        'group_id': chat,
+                        'reason': H['arg']
+                    })
+
+                    buttons = [Button.inline("⚠️ Remove warn", 'remove_warn_{}'.format(rndm))]
+                    rules = mongodb.rules.find_one({"chat_id": chat})
+
+                    if rules:
+                        buttons.append(Button.inline("📝 Rules", 'get_note_{}_{}'.format(
+                            chat, rules['note']
+                        )))
+
+                    status, chat_id, chat_title = await get_conn_chat(event.from_id, event.chat_id,
+                                                                      admin=True, only_in_groups=True)
+
+                    resp = get_string("filters", "filter_warn_warned", event.chat_id).format(user=await user_link(user),
+                                                                                             chat=chat_title,
+                                                                                             current_warns=current_warns,
+                                                                                             max_warns=warn_limit,
+                                                                                             reason=H['arg'])
+                    return await event.reply(resp, buttons=buttons)
             break
 
 
@@ -109,6 +180,15 @@ async def add_filter(event, strings, status, chat_id, chat_title):
         text += strings["a_mute"]
     elif action == 'kick':
         text += strings["a_kick"]
+    elif action == 'warn':
+        raw_text = event.text.split(" ")
+        arg = None
+        if raw_text[3:]:
+            arg = " ".join(raw_text[3:])
+        else:
+            arg = f"Automatic action on filter:\n{handler.lower()}."
+
+        text += strings["a_warn"].format(arg)
     else:
         await event.reply(strings["wrong_action"])
         return
