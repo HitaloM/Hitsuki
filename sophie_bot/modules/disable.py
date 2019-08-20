4dfa272c21317fa67007f96194a6caa08e7f5456
MrYacha <YachaGorobtsov@gmail.com>
2019-08-20 18:52:34 +0300

    Add missing GNU and copyrights headers

--- sophie_bot/modules/disable.py
+++ sophie_bot/modules/disable.py
@@ -1,3 +1,18 @@
+# Copyright © 2018, 2019 MrYacha
+# This file is part of SophieBot.
+#
+# SophieBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SophieBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
 from sophie_bot import decorator, mongodb
 from sophie_bot.modules.connections import connection
 from sophie_bot.modules.language import get_strings_dec
