95151e03e031a037fd067efcbdfd92fdbf81d91d
MrYacha <YachaGorobtsov@gmail.com>
2019-09-04 09:15:46 +0000

    Fix unmuted rights

--- sophie_bot/modules/bans.py
+++ sophie_bot/modules/bans.py
@@ -293,7 +293,10 @@ async def unmute_user(message, user_id, chat_id):
         await bot.restrict_chat_member(
             chat_id,
             user_id,
-            can_send_messages=True
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True
         )
     except NotEnoughRightsToRestrict:
         raise NotEnoughRights('unmute')
