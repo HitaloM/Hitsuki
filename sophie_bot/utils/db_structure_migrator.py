0baa9be1c07942bcd38eafc72e788822107525e5
MrYacha <YachaGorobtsov@gmail.com>
2019-12-06 22:16:31 +0200

    DB Structure migrator: Reduce logging

--- sophie_bot/utils/db_structure_migrator.py
+++ sophie_bot/utils/db_structure_migrator.py
@@ -32,23 +32,23 @@ async def notify_bot_owner(old_ver, new_ver):
 log.debug("Checking on database structure update...")
 
 if not (data := mongodb.db_structure.find_one({'db_ver': {"$exists": True}})):
-    log.warning("Your database is empty! Creating database structure key...")
+    log.info("Your database is empty! Creating database structure key...")
     mongodb.db_structure.insert_one({'db_ver': DB_STRUCTURE_VER})
-    log.info("Your database structure version is: " + str(DB_STRUCTURE_VER))
+    log.info("Database structure version is: " + str(DB_STRUCTURE_VER))
 else:
     curr_ver = data['db_ver']
     log.info("Your database structure version is: " + str(curr_ver))
     if DB_STRUCTURE_VER > curr_ver:
-        log.critical("Your database is old! Waiting 20 seconds till update...")
-        log.warn("Press CTRL + C to cancel!")
+        log.error("Your database is old! Waiting 20 seconds till update...")
+        log.info("Press CTRL + C to cancel!")
         time.sleep(20)
-        log.warn("Let's backup DB for first.")
+        log.debug("Let's backup DB for first.")
         backup_db(postfix='_pre-update-structure')
-        log.warn("Trying to update database structure...")
-        log.warn("--------------------------------")
+        log.debug("Trying to update database structure...")
+        log.info("--------------------------------")
         log.info("Your current database structure version: " + str(curr_ver))
         log.info("New database structure version: " + str(DB_STRUCTURE_VER))
-        log.warn("--------------------------------")
+        log.info("--------------------------------")
         old_ver = curr_ver
         while curr_ver < DB_STRUCTURE_VER:
             new_ver = curr_ver + 1
@@ -60,10 +60,10 @@ else:
             curr_ver += 1
             mongodb.db_structure.update_one({'db_ver': curr_ver - 1}, {"$set": {'db_ver': curr_ver}})
 
-        log.info(f"Database update done to {str(curr_ver)} successfully!")
+        log.warn(f"Database update done to {str(curr_ver)} successfully!")
         log.debug("Let's notify the bot owner")
         loop = asyncio.get_event_loop()
         bot_info = loop.run_until_complete(notify_bot_owner(old_ver, curr_ver))
         log.info("Rescue normal bot startup...")
     else:
-        log.info("No database structure updates found, skipping!")
+        log.debug("No database structure updates found, skipping!")
