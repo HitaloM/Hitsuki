43bd04fb2322e1c6111906132d26fca0b55da6e7
MrYacha <YachaGorobtsov@gmail.com>
2020-03-29 20:21:27 +0300

    Change license to AGPL

--- sophie_bot/modules/utils/tmarkdown.py
+++ sophie_bot/modules/utils/tmarkdown.py
@@ -1,13 +1,23 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-# Copyright (C) 2018 - 2020 MrYacha
-# Copyright (C) 2017 - 2020 Aiogram
+# Copyright (C) 2018 - 2020 MrYacha. All rights reserved. Source code available under the AGPL.
+# Copyright (C) 2019 Aiogram
 # Copyright (C) 2017 - 2020 Telethon
 
-"""
-Simple markdown parser which does not support nesting. Intended primarily
-for use within the library, which attempts to handle emojies correctly,
-since they seem to count as two characters and it's a bit strange.
-"""
+#
+# This file is part of SophieBot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import warnings
 
