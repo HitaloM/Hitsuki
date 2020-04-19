c13cc9e58fc0a13cbbe9f057181091128647fd02
sabby <sabby.6@outlook.com>
2020-04-19 20:05:39 +0530

    error_handling: Improved privacy & avoid spamming same error
    Signed-off-by: sabby <sabby.6@outlook.com>

--- sophie_bot/modules/error.py
+++ sophie_bot/modules/error.py
@@ -21,29 +21,34 @@ import html
 import sys
 
 from sophie_bot import dp, bot
+from sophie_bot.services.redis import redis
 from sophie_bot.utils.logger import log
 
 
 @dp.errors_handler()
 async def all_errors_handler(message, dp):
-    if 'callback_query' in message:
-        msg = message.callback_query.message
-    else:
-        msg = message.message
+    msg = message.callback_query.message if 'callback_query' in message else message.message
     chat_id = msg.chat.id
     err_tlt = sys.exc_info()[0].__name__
     err_msg = str(sys.exc_info()[1])
+
+    if redis.get(chat_id) == err_tlt:
+        # by err_tlt we assume that it is same error
+        return
 
     if err_tlt == 'BadRequest' and err_msg == 'Have no rights to send a message':
         return True
 
     text = "<b>Sorry, I encountered a error!</b>\n"
     text += f'<code>{html.escape(err_tlt)}: {html.escape(err_msg)}</code>'
+    redis.set(chat_id, err_tlt, ex=120)
     await bot.send_message(chat_id, text, reply_to_message_id=msg.message_id)
 
     # Protect Privacy
     msg['chat'] = ['HIDDEN']
     msg['from'] = ['HIDDEN']
     msg['message_id'] = ['HIDDEN']
+    if hasattr(msg, 'reply_to_message'):
+        msg['reply_to_message'] = ['HIDDEN']
 
     log.error('Error caused update is: \n' + str(msg))
