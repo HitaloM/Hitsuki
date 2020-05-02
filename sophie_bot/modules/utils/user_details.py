8ebd43fd8fc382746b59fba63214485046280e36
sabby <sabby.6@outlook.com>
2020-05-02 13:19:38 +0530

    utils: don't force update cache by default
    Signed-off-by: sabby <sabby.6@outlook.com>

--- sophie_bot/modules/utils/user_details.py
+++ sophie_bot/modules/utils/user_details.py
@@ -123,7 +123,7 @@ async def get_user_link(user_id, custom_name=None, md=False):
         return "<a href=\"tg://user?id={id}\">{name}</a>".format(name=user_name, id=user_id)
 
 
-async def get_admins_rights(chat_id, force_update=True):
+async def get_admins_rights(chat_id, force_update=False):
     key = 'admin_cache:' + str(chat_id)
     if alist := bredis.get(key) and not force_update:
         return pickle.loads(alist)
