0e4fd9c956113719b59047a061610aa083d69d7b
sabby <sabby.6@outlook.com>
2020-05-03 14:30:33 +0530

    greetings: fix bug in getting username
    Signed-off-by: sabby <sabby.6@outlook.com>

--- sophie_bot/modules/utils/notes.py
+++ sophie_bot/modules/utils/notes.py
@@ -384,8 +384,9 @@ async def vars_parser(text, message, chat_id, md=False, event=None):
     user_id = ([user.id for user in event.new_chat_members][0]
                if event.new_chat_members != [] else event.from_user.id)
     mention = await get_user_link(user_id, md=md)
-    username = ('@' + str([user.username for user in event.new_chat_members])
-                if event.new_chat_members != [] else '@' + event.from_user.username
+    username = ('@' + str(event.new_chat_members[0].username)
+                if event.new_chat_members != [] and event.new_chat_members[0].username is not None
+                else '@' + event.from_user.username
                 if event.from_user.username is not None else mention)
     chat_id = message.chat.id
     chat_name = html.escape(message.chat.title or 'Local')
