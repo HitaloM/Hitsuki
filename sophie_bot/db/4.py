1578ee9beb55281f31911a5f311f556eb6d040b7
MrYacha <YachaGorobtsov@gmail.com>
2020-05-14 02:01:07 +0300

    DB Structure: Fix filters migrations: 'note' to 'note_name'

--- sophie_bot/db/4.py
+++ sophie_bot/db/4.py
@@ -16,5 +16,27 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-SOPHIE_VERSION = "v2.0.2"
-DB_STRUCTURE_VER = 4
+from sophie_bot.services.mongo import mongodb
+from sophie_bot.utils.logger import log
+
+log.info('Sophie Database v3')
+log.info("Filters: move 'note' to 'note_name'")
+log.info('Starting updating all filters...')
+
+all_filters = mongodb.filters.find({})
+all_filters_count = all_filters.count()
+counter = 0
+changed_filters = 0
+for item in all_filters:
+    counter += 1
+    log.info(f'Updating {counter} of {all_filters_count}...')
+
+    if 'note' in item:
+        changed_filters += 1
+        item['note_name'] = item['note']
+        del item['note']
+        mongodb.notes_v2.replace_one({'_id': item['_id']}, item)
+
+log.info('Update done!')
+log.info('Modified filters - ' + str(changed_filters))
+log.info('Unchanged filters - ' + str(all_filters_count - changed_filters))
