845de009b515f1df1a6e7a2e8f8f4110c46117c6
sabby <sabby.6@outlook.com>
2020-06-15 09:29:00 +0530

    promotes: 🐛 add exception when entity is not found
    * fixes when promoting username-less users without title

--- sophie_bot/modules/promotes.py
+++ sophie_bot/modules/promotes.py
@@ -18,8 +18,8 @@ from aiogram.utils.exceptions import ChatAdminRequired
 
 from .utils.connections import chat_connection
 from .utils.language import get_strings_dec
-from .utils.user_details import get_user_dec, get_user_link, get_admins_rights
-from .utils.message import get_args_str
+from .utils.user_details import (get_user_dec, get_user_and_text_dec,
+                                 get_user_link, get_admins_rights)
 
 from sophie_bot.decorator import register
 from sophie_bot.services.telethon import tbot
@@ -28,9 +28,9 @@ from sophie_bot import BOT_ID, bot
 
 @register(cmds="promote", bot_can_promote_members=True, user_can_promote_members=True)
 @chat_connection(admin=True, only_groups=True)
-@get_user_dec()
+@get_user_and_text_dec()
 @get_strings_dec('promotes')
-async def promote(message, chat, user, strings):
+async def promote(message, chat, user, args, strings):
     chat_id = chat['chat_id']
     text = strings['promote_success'].format(
         user=await get_user_link(user['user_id']),
@@ -42,24 +42,27 @@ async def promote(message, chat, user, strings):
 
     title = None
 
-    if len(arg := get_args_str(message)) > 0:
-        if len(arg) > 16:
+    if args:
+        if len(args) > 16:
             await message.reply(strings['rank_to_loong'])
             return
-        title = arg
+        title = args
         text += strings['promote_title'].format(role=title)
 
-    await tbot.edit_admin(
-        chat_id,
-        user['user_id'],
-        add_admins=True,
-        invite_users=True,
-        change_info=True,
-        ban_users=True,
-        delete_messages=True,
-        pin_messages=True,
-        title=title
-    )
+    try:
+        await tbot.edit_admin(
+            chat_id,
+            user['user_id'],
+            add_admins=True,
+            invite_users=True,
+            change_info=True,
+            ban_users=True,
+            delete_messages=True,
+            pin_messages=True,
+            title=title
+        )
+    except ValueError:
+        return await message.reply(strings['cant_get_user'])
     await get_admins_rights(chat_id, force_update=True)  # Reset a cache
     await message.reply(text)
 
