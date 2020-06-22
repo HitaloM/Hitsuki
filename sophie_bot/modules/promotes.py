89c5d09ae902ffbd3fc9289acdfd301486689bf5
sabby <sabby.6@outlook.com>
2020-06-22 08:37:04 +0530

    promotes: 🐛 escape HTML from title (sending message)

--- sophie_bot/modules/promotes.py
+++ sophie_bot/modules/promotes.py
@@ -14,6 +14,8 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import html
+
 from aiogram.utils.exceptions import ChatAdminRequired
 
 from .utils.connections import chat_connection
@@ -49,7 +51,7 @@ async def promote(message, chat, user, args, strings):
             await message.reply(strings['rank_to_loong'])
             return
         title = args
-        text += strings['promote_title'].format(role=title)
+        text += strings['promote_title'].format(role=html.escape(title))
 
     try:
         await tbot.edit_admin(
