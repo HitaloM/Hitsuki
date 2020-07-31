c7288113f1ddef0c86b4b39523e2f1165188fc63
sabby <sabby.6@outlook.com>
2020-07-31 13:17:27 +0530

    feds: fix error when reason wasnt given at first time

--- sophie_bot/modules/feds.py
+++ sophie_bot/modules/feds.py
@@ -543,9 +543,10 @@ async def fed_ban_user(message, fed, user, reason, strings):
         return
 
     elif data := await db.fed_bans.find_one({'fed_id': fed['fed_id'], 'user_id': user_id}):
-        if data['reason'] != reason:
-            await db.fed_bans.update_one({'_id': data['_id']}, {'$set': {'reason': reason}})
-            return await message.reply(strings['update_fban'].format(reason=reason))
+        if reason:  # Check if reason is given
+            if 'reason' not in data or data["reason"] != reason:
+                await db.fed_bans.update_one({'_id': data['_id']}, {'$set': {'reason': reason}})
+                return await message.reply(strings['update_fban'].format(reason=reason))
         await message.reply(strings['already_fbanned'].format(user=await get_user_link(user_id)))
         return
 
