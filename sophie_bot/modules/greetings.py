35e1f58b8eb11d5b1149eba41b2ca4c318b2dbac
sabby <sabby.6@outlook.com>
2020-08-11 21:05:42 +0530

    hotfix: fix join expiration

--- sophie_bot/modules/greetings.py
+++ sophie_bot/modules/greetings.py
@@ -429,7 +429,7 @@ async def welcome_security_handler(message, strings):
 
 async def join_expired(chat_id, user_id, message_id, wlkm_msg_id):
     user = await bot.get_chat_member(chat_id, user_id)
-    if 'can_send_messages' not in user or user['can_send_messages'] is True:
+    if user.can_send_messages is True:
         return
 
     bot_user = await bot.get_chat_member(chat_id, BOT_ID)
