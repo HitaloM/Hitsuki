969862d6a71238a501e7b1de260db14ea13e49b7
sabby <sabby.6@outlook.com>
2020-10-23 16:13:13 +0530

    connections: show disclaimer when having bridged commands

--- sophie_bot/modules/connection.py
+++ sophie_bot/modules/connection.py
@@ -83,23 +83,33 @@ async def connect_to_chat_direct(message, strings):
 @get_strings_dec('connections')
 @chat_connection()
 async def connect_chat_keyboard(message, strings, chat):
+    connected_data = await get_connection_data(message.from_user.id)
+    if not connected_data:
+        return await message.reply(strings['u_wasnt_connected'])
+
     if chat['status'] != 'private':
         text = strings['connected_chat'].format(chat_name=chat['chat_title'])
+    elif 'command' in connected_data:
+        if chat := await db.chat_list.find_one({'chat_id': connected_data['chat_id']}):
+            chat_title = chat['chat_title']
+        else:
+            chat_title = connected_data['chat_id']
+        text = strings['connected_chat:cmds'].format(
+            chat_name=chat_title,
+            # disconnect is builtin command, should not be shown
+            commands=", ".join(f"<code>/{cmd}</code>" for cmd in connected_data['command'] if cmd != "disconnect")
+        )
     else:
         text = ''
 
     text += strings['select_chat_to_connect']
     markup = InlineKeyboardMarkup(row_width=1)
-
-    if connected := await db.connections.find_one({'user_id': message.from_user.id}, {'history': {'$slice': -3}}):
-        for chat_id in reversed(connected['history']):
-            chat = await db.chat_list.find_one({'chat_id': chat_id})
-            markup.insert(InlineKeyboardButton(
-                chat['chat_title'],
-                callback_data=connect_to_chat_cb.new(chat_id=chat_id))
-            )
-    else:
-        return await message.reply(strings['u_wasnt_connected'])
+    for chat_id in reversed(connected_data['history'][-3:]):
+        chat = await db.chat_list.find_one({'chat_id': chat_id})
+        markup.insert(InlineKeyboardButton(
+            chat['chat_title'],
+            callback_data=connect_to_chat_cb.new(chat_id=chat_id))
+        )
 
     await message.reply(text, reply_markup=markup)
 
