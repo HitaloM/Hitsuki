784c07ed0b7153424f1487121082987fae955c5d
sabby <sabby.6@outlook.com>
2020-11-23 12:58:22 +0530

    bump to v2.2.2

--- sophie_bot/versions.py
+++ sophie_bot/versions.py
@@ -16,5 +16,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-SOPHIE_VERSION = "v2.2.1"
+SOPHIE_VERSION = "v2.2.2"
 DB_STRUCTURE_VER = 8
