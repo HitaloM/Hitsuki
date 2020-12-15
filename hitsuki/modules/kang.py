7cd533b1b59bab2ac7e12eb4cc4508d05b04888c
HitaloSama <hitalo331@outlook.com>
2020-12-15 19:52:30 +0000

    kang: added Hitsuki's license header

--- hitsuki/modules/kang.py
+++ hitsuki/modules/kang.py
@@ -1,12 +1,28 @@
+# This file is part of Hitsuki (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
 from aiogram import types
+from PIL import Image
+from math import floor
+
 from hitsuki import bot
 from hitsuki.decorator import register
 from .utils.disable import disableable_dec
-from PIL import Image
-from math import floor
-import os
 
-download_path = "/"
+download_path = "./"
 
 
 class Stickers:
