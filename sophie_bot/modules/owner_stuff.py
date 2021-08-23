0843018ed7775a960847727c7b9a4dc010a72ccb
Yacha <yacha@orangefox.tech>
2021-08-23 09:25:48 +0000

    Update sophie_bot/modules/owner_stuff.py

--- sophie_bot/modules/owner_stuff.py
+++ sophie_bot/modules/owner_stuff.py
@@ -134,7 +134,7 @@ async def purge_caches(message):
     await message.reply("Redis cache was cleaned.")
 
 
-@register(cmds="botstop", is_owner=True)
+@register(cmds="stopbot", is_owner=True)
 async def bot_stop(message):
     await message.reply("Goodbye...")
     exit(1)
