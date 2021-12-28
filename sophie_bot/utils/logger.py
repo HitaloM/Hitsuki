269aa36f58c12867205cd2168816dd300b313ec1
Yacha <yacha@orangefox.tech>
2021-12-29 01:29:26 +0200

    Disable logging to files

--- sophie_bot/utils/logger.py
+++ sophie_bot/utils/logger.py
@@ -40,5 +40,3 @@ class InterceptHandler(logging.Handler):
 
 logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
 log = logging.getLogger(__name__)
-logger.add("logs/sophie.log", rotation="1 d", compression="tar.xz", backtrace=True, diagnose=True, level="INFO")
-log.info("Enabled logging intro sophie.log file.")
