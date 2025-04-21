276eafc9cc0ec30ff89c42928e07cdb171339882
Hitalo M. <hitalo331@outlook.com>
2025-04-21 12:13:25 -0300

    refactor(config): update load method to async and remove unused serialize

--- src/config.rs
+++ src/config.rs
@@ -2,25 +2,27 @@
 // Copyright (c) 2025 Hitalo M. <https://github.com/HitaloM>
 
 use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
-use std::fs;
+use serde::Deserialize;
+use tokio::fs;
 
 const PATH: &str = "./config.toml";
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize)]
 pub struct Config {
     pub bot: Bot,
 }
 
 impl Config {
-    pub fn load() -> Result<Self> {
-        let toml_str = fs::read_to_string(PATH).context("Failed to read config file")?;
+    pub async fn load() -> Result<Self> {
+        let toml_str = fs::read_to_string(PATH)
+            .await
+            .context("Failed to read config file")?;
         let config = toml::from_str::<Config>(&toml_str).context("Failed to parse config file")?;
         Ok(config)
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize)]
 pub struct Bot {
     pub token: String,
     pub database_url: String,
